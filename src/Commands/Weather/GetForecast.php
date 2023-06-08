83c07038953f8e7c2214312235bc6b688d0c3242
Justin <justinjongstra8@gmail.com>
2023-06-08 16:12:29 +0200

    added weather alerts to forecast

--- src/Commands/Weather/GetForecast.php
+++ src/Commands/Weather/GetForecast.php
@@ -69,29 +69,56 @@ class GetForecast
         $avgHumidity = 0;
 
         foreach ($forecast as $day) {
+            // Check if there are any alerts for the day
+            if (isset($day['alerts'])) {
+                $alert = $day['alerts'][0];
+                $alert['date'] = date('l \t\h\e jS \o\f F', strtotime($alert['date']));
+            }
+
+            $alertField = isset($day['alerts']) ? '- ' : '+ ';
+            $alertMessage = isset($day['alerts']) ? $alert['event'] : 'No alerts';
+
             $embedFields[] = [
                 'name' => $day['date'],
-                'value' => "Max temp: {$day['day']['maxtemp_c']}°C\nMin temp: {$day['day']['mintemp_c']}°C\nAverage temp: {$day['day']['avgtemp_c']}°C\nMax wind speed: {$day['day']['maxwind_kph']}kph\nTotal precipitation: {$day['day']['totalprecip_mm']}mm\nAverage humidity: {$day['day']['avghumidity']}%\nCondition: {$day['day']['condition']['text']}",
+                'value' => "```diff
+Max temp: {$day['day']['maxtemp_c']}°C
+Min temp: {$day['day']['mintemp_c']}°C
+Average temp: {$day['day']['avgtemp_c']}°C
+Max wind speed: {$day['day']['maxwind_kph']}kph
+Total precipitation: {$day['day']['totalprecip_mm']}mm
+Average humidity: {$day['day']['avghumidity']}%
+Condition: {$day['day']['condition']['text']}
+{$alertField}Alert: {$alertMessage}
+```",
                 'inline' => false
             ];
+
 
             $avgTemp += $day['day']['avgtemp_c'];
             $avgHumidity += $day['day']['avghumidity'];
         }
+
         $builder->addField('Average temp (3 days)', round($avgTemp / 3, 2) . '°C', true);
         $builder->addField('Average humidity (3 days)', round($avgHumidity / 3, 2) . '%', true);
         $builder->addLineBreak();
 
         foreach ($forecast as $day) {
             $day['date'] = date('l \t\h\e jS \o\f F', strtotime($day['date']));
-            $builder->addField('Condition for: ' . $day['date'], $day['day']['condition']['text'] . " " . $day['day']['avgtemp_c'] . '°C ' . $day['day']['totalprecip_mm'] . 'mm rain', false);
+            $builder->addField(
+                'Condition for: ' . $day['date'],
+                isset($day['alerts']) ? $day['alerts'][0]['event'] : "```
+Condition: {$day['day']['condition']['text']}
+Average temp: {$day['day']['avgtemp_c']}°C
+Total precipitation: {$day['day']['totalprecip_mm']}mm rain
+```",
+                false
+            );
         }
 
         $messageBuilder = MessageBuilder::buildMessage($builder);
         $slashIndex = new SlashIndex($embedFields);
         $slashIndex->setTotalPerPage(1);
         $slashIndex->handlePagination(count($embedFields), $messageBuilder, $discord, $interaction, $builder, $title, '', '', true);
+
     }
-
-
 }
